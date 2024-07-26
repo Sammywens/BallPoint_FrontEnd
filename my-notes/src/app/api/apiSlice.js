6fe5590f3005c0ce9e352d5d5fd2ab252a3937cf
Sammywens <nwene.samuel@gmail.com>
2024-07-26 16:17:11 +0100

    :wrench: Frontend

--- my-notes/src/app/api/apiSlice.js
+++ my-notes/src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://stonegrowth-api.onrender.com',
+    baseUrl: 'https://BallPoint-api.onrender.com',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
